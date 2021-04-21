4ed38dc093c3668f651307533a68442957f89515
Pedro Figueiredo <pedrofigueiredo9@gmail.com>
2021-04-21 14:26:35 -0300

    Added Bamboo dependency for sending e-mails.

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule Inmana.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.24", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.24", only: :dev, runtime: false},
+      {:bamboo, "~> 2.1.0"}
     ]
   end
 
